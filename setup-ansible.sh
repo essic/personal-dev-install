37c73733fd90de8081b9bb5079e476d6a64ca740
essic <lollancf37@gmail.com>
2022-11-10 18:56:33 +0100

    fix: installation script

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -3,4 +3,5 @@
 sudo apt install -y python3-venv python3-pip
 pip3 install ansible
 pip3 install ansible-lint
-ansible-galaxy install diodonfrost.terraform
+$HOME/.local/bin/ansible-galaxy install diodonfrost.terraform
+$HOME/.local/bin/ansible-galaxy collection install community.general
