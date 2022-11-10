cbb2b34ca664a4f5b1db9689dcc57792ff4b5f31
Aly-Bocar Cissé <essic@users.noreply.github.com>
2022-11-10 20:55:15 +0100

    chore: update installation

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -3,3 +3,5 @@
 sudo apt install -y python3-venv python3-pip
 pip3 install ansible
 pip3 install ansible-lint
+$HOME/.local/bin/ansible-galaxy install diodonfrost.terraform
+$HOME/.local/bin/ansible-galaxy collection install community.general
