6aaf852c95590806689262b21e592285f99d32cc
essic <lollancf37@gmail.com>
2024-10-25 20:52:50 +0200

    chore: add some tools, mdless included

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt install -y python3-venv python3-pip
-pip3 install --break-system-packages ansible
-pip3 install --break-system-packages ansible-lint
+pip3 install ansible
+pip3 install ansible-lint
 $HOME/.local/bin/ansible-galaxy install diodonfrost.terraform
 $HOME/.local/bin/ansible-galaxy collection install community.general
 $HOME/.local/bin/ansible-galaxy install gantsign.visual-studio-code
